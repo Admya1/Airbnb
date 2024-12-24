6d86c744f5569afa50a05a1eb13e405af6e6bb7f
Admya1 <admyamaini@gmail.com>
2024-12-24 21:56:15 +0530

    Tailwind css and header

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,13 +1,21 @@
+/** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
-
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*',
   ],
   theme: {
+    colors: {
+      ...colors,
+      primary: {
+        DEFAULT: '#FF5A5F',
+        dark: '#FF385C',
+      }
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],

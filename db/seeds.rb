d26dffce30fd58bda21ead02015d322b45a74b37
Admya1 <admyamaini@gmail.com>
2024-12-25 13:23:16 +0530

    designed sign up and logi in pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Property.create!({
+    name:'xyz',
+    headline:'xyzabc',
+    description:'asdfgh',
+    address_1:'asdfs',
+    city:'sdgff',
+    state:'asf',
+    country:'sgafw',
+})
